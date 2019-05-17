fb0bf018966dfe8d254fb78a84152bbd33a6b9c7
lcripl <crip@list.ru>
2019-05-17 19:39:55 +0600

    init commit

---
+++ src/javascripts/scripts.js
@@ -0,0 +1,43 @@
+// Add your scripts here
+import * as jQuery from 'jquery';
+import 'bootstrap';
+// export for others scripts to use
+
+// workaround for main-carousel-counter
+jQuery(document).ready(($) => {
+  if ($('.main-carousel-counter').length) {
+    const counter = $('.main-carousel-counter');
+    const summ = counter.siblings('.main-carousel-indicators').children().length;
+    const summText = counter.find('.main-carousel-summ');
+    const summIndex = counter.find('.main-carousel-index');
+
+    summText.text(() => {
+      const text = summ < 10 ? `0${summ}` : summ;
+      return text;
+    });
+
+    $('#main-carousel').on('slide.bs.carousel', (event) => {
+      const index = event.to + 1;
+      const digit = index < 10 ? `0${index}` : index;
+      const newSpan = $('<span></span>');
+      const oldSpan = summIndex.find('span');
+
+      oldSpan
+        .removeClass('in')
+        .addClass('out')
+        .delay(1000)
+        // eslint-disable-next-line func-names
+        .queue(function (next) {
+          $(this).remove();
+          next();
+        });
+      newSpan.text(digit);
+      summIndex.prepend(newSpan);
+      // eslint-disable-next-line func-names
+      newSpan.delay(100).queue(function (next) {
+        $(this).addClass('in');
+        next();
+      });
+    });
+  }
+});
